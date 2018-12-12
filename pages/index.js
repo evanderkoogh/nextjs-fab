7c7ce7d5132af00c3832c350c8f7b976449859ed
Glen Maddern <glenmaddern@gmail.com>
2018-12-12 21:23:02 +1100

    oh I am so glad this worked first time

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-export default () => <div>Welcome to next.js on FAB!</div>
+import Link from 'next/link'
+
+export default () => (
+  <div>
+    <h1>Next.js on FAB! 👋</h1>
+    <Link href='/about'><a>About</a></Link>
+  </div>
+)
