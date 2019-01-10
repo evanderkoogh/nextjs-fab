e445ab4260f376c4b617e8026095dbec15b09e0d
Glen Maddern <glenmaddern@gmail.com>
2019-01-10 15:39:16 +1100

    testing that this still works

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default () => (
   <div>
-    <h1>Next.js on FAB! 👋</h1>
+    <h1>Next.js is still on FAB! 👋</h1>
     <Link href='/about'><a>About</a></Link>
   </div>
 )
