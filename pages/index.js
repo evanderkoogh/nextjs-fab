d9bd5a77e0abcf9af7889720c95e0590152bab40
Erwin van der Koogh <erwin@bitgenics.io>
2019-02-07 13:15:19 +1100

    Hello Thinkmill

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default () => (
   <div>
-    <h1>Hello World! 👋</h1>
+    <h1>Hello ThinkMill! 👋</h1>
     <Link href="/about">
       <a>Can he be this cliche?</a>
     </Link>
